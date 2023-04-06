761725efed483ead1765e68f89e644cc11806de8
Duotech B4 <duotechb3@gmail.com>
2023-04-05 22:03:55 -0400

    parallel testing implemented

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -7,7 +7,10 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
+
+import java.net.URL;
 
 public class Driver {
 
@@ -62,6 +65,7 @@ public class Driver {
                 case "safari":
                     drivers.set(new SafariDriver());
                     break;
+                    
                 default:
                     throw new RuntimeException("Unsupported browser");
 
