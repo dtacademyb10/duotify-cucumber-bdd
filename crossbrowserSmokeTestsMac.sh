7b3b51dacf5af151247d533494804cefa46aad73
Duotech B4 <duotechb3@gmail.com>
2023-04-05 23:52:11 -0400

    added fix for multiple reports for each browser in crossbrowser tests

--- crossbrowserSmokeTestsMac.sh
+++ crossbrowserSmokeTestsMac.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mvn test -D cucumber.filter.tags="@smoke" -D env=QA -D browser=chrome
-mvn test -D cucumber.filter.tags="@smoke" -D env=QA -D browser=edge
-mvn test -D cucumber.filter.tags="@smoke" -D env=QA -D browser=firefox
+mvn test -D cucumber.filter.tags="@smoke" -D env=QA -D browser=chrome -D cucumber.plugin="html:target/basic-report/reportChrome.html"
+mvn test -D cucumber.filter.tags="@smoke" -D env=QA -D browser=edge -D cucumber.plugin="html:target/basic-report/reportEdge.html"
+mvn test -D cucumber.filter.tags="@smoke" -D env=QA -D browser=firefox -D cucumber.plugin="html:target/basic-report/reportFirefox.html"
