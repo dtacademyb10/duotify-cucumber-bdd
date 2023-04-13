b2aedc336829e9f09cc379b71925d3ed448d5475
DuotechA <duotech@gmail.com>
2023-04-12 22:11:20 -0400

    business rules testing more examples

--- src/test/java/stepDefinitions/db/BusinesRulesStepDefs.java
+++ src/test/java/stepDefinitions/db/BusinesRulesStepDefs.java
@@ -5,10 +5,7 @@ import io.cucumber.java.en.When;
 import org.junit.Assert;
 import utils.DBUtils;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BusinesRulesStepDefs {
 
@@ -32,4 +29,48 @@ public class BusinesRulesStepDefs {
 
         Assert.assertEquals(expected, actual);
     }
+
+    List<String> actualColumnNames;
+    @When("I send a request to retrieve column names for albums table")
+    public void i_send_a_request_to_retrieve_column_names_for_albums_table() {
+
+        actualColumnNames = DBUtils.getColumnNames("SELECT * from albums");
+
+    }
+    @Then("It should be the following")
+    public void it_should_be_the_following(List<String> expectedColumnNames) {
+
+        Assert.assertEquals(expectedColumnNames, actualColumnNames);
+    }
+
+    List<String> list;
+    @When("I send a request to retrieve usernames")
+    public void i_send_a_request_to_retrieve_usernames() {
+        list = DBUtils.getSingleColumnValues("username", "users");
+
+    }
+    @Then("the result should contain duplicates")
+    public void the_result_should_contain_duplicates() {
+
+        ArrayList<String> actual = new ArrayList<>(new LinkedHashSet<>(list));
+
+        System.out.println(actual);
+        System.out.println(list);
+
+        Assert.assertEquals(list, actual);
+
+    }
+
+    List<Map<String, Object>> expectedList;
+    @When("I send a request to retrieve duplicate usernames")
+    public void i_send_a_request_to_retrieve_duplicate_usernames() {
+        expectedList = DBUtils.getListOfMaps("SELECT  username,count(*) from users GROUP BY username having count(*) > 1");
+    }
+    @Then("The result should be empty")
+    public void the_result_should_be_empty() {
+         Assert.assertEquals(0, expectedList.size());
+    }
+
+
+
 }
--- src/test/resources/db/business-rules.feature
+++ src/test/resources/db/business-rules.feature
@@ -1,7 +1,7 @@
 @DB
 Feature: Verify database related business rules
 
- @db_only
+
   Scenario: Verify the pre-defined genres in the database
     When  I send a request to retrieve genres from genres table
     Then It should be the following genres
@@ -17,3 +17,27 @@ Feature: Verify database related business rules
       | reggae     |
       | reggaeton  |
       | rap        |
+
+  Scenario: Verify the column names for albums table
+    When I send a request to retrieve column names for albums table
+    Then It should be the following
+      | id          |
+      | title       |
+      | artist      |
+      | genre       |
+      | artworkPath |
+
+
+  Scenario: Verify the duplicate usernames
+    When  I send a request to retrieve usernames
+    Then  the result should contain duplicates
+
+
+  @db_only
+  Scenario: Verify the duplicate usernames
+    When  I send a request to retrieve duplicate usernames
+    Then  The result should be empty
+
+#
+
+
